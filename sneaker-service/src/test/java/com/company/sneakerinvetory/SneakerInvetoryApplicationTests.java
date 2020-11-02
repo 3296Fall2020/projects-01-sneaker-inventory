e82a8c8292e39522abfce524405a59a8a4e06087
Terence21 <terencepeterson21@gmail.com>
2020-11-01 20:35:22 -0800

    hello world spring boot

---
+++ sneaker-service/src/test/java/com/company/sneakerinvetory/SneakerInvetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.sneakerinvetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerInvetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
