0a6acbe0ef096352bb37d8f01e7e8c650705cfec
McCoderFace3000 <59894218+McCoderFace3000@users.noreply.github.com>
2020-11-05 17:23:32 -0500

    Update DatabaseOperation.java
    updated to make addUser a boolean

--- MySQLConnction/src/DatabaseOperation.java
+++ MySQLConnction/src/DatabaseOperation.java
@@ -4,11 +4,11 @@ public class DatabaseOperation {
     Connection connect; // where connection to database will be stored on createConnection call
     Statement statement; // interface to represent SQL statement
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main used for testing
         DatabaseOperation database = new DatabaseOperation();
         database.createConnect(); // create connect, needs to be done it initialize connect variable
         //database.printUsers();
-        database.addUser("Tony", "password"); // register
+        System.out.println(database.addUser("Tony", "password")); // register
         System.out.println(database.signIn("Akeem","password")); // login
         database.closeConnection(); // log off
     }
@@ -59,13 +59,14 @@ public class DatabaseOperation {
         }
     }
 
-    void addUser(String userID, String password) {
+    boolean addUser(String userID, String password) {
         try {
             statement = connect.createStatement(); // create statement to be exicuted on connection
             statement.executeUpdate("INSERT INTO user_table  VALUES('" + userID + "','" + password + "')");
+            return true;
         } catch (SQLException throwables) {
             //System.out.println("User already exists");
-            return;
+            return false;
         }
         //System.out.println("User added");
     }
