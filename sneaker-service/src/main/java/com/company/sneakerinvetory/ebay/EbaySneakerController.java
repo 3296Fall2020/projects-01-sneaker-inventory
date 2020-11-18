a3913cf0333f9ef94322a587972afb95152183ac
Terence21 <terencepeterson21@gmail.com>
2020-11-18 12:08:23 -0800

    changed poor names

--- sneaker-service/src/main/java/com/company/sneakerinvetory/ebay/EbaySneakerController.java
+++ sneaker-service/src/main/java/com/company/sneakerinvetory/ebay/EbaySneakerController.java
@@ -1,13 +1,13 @@
 package com.company.sneakerinvetory.ebay;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.RequestBody;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RestController;
 
 // curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X GET --data '{ "sku" : "fu9006", "size" : "11", "name": "yeezy 350 black"}' "http://localhost:8080/ebayAverage"
 @RestController
-public class EbayEndpoint {
+public class EbaySneakerController {
 
     @RequestMapping(value = "/ebayAverage", method = RequestMethod.GET)
     public EbayResponse handleEbayAverage(@RequestBody EbaySneaker ebaySneaker){
