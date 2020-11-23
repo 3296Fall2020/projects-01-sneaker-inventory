5fbfe4167b8ab24c41578f2fbfbd7677d2791909
Terence21 <terencepeterson21@gmail.com>
2020-11-23 14:03:08 -0800

    added redirect to all endpoints... still need to update to hosting website

--- sneaker-service/src/main/java/com/company/sneakerinvetory/ebay/EbaySneakerController.java
+++ sneaker-service/src/main/java/com/company/sneakerinvetory/ebay/EbaySneakerController.java
@@ -1,23 +1,33 @@
 package com.company.sneakerinvetory.ebay;
 
+        import com.company.sneakerinvetory.HelloController;
         import org.springframework.web.bind.annotation.RequestBody;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestMethod;
         import org.springframework.web.bind.annotation.RestController;
+
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import java.io.IOException;
 
 // curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X GET --data '{ "sku" : "fu9006", "size" : "11", "name": "yeezy 350 black"}' "http://localhost:8080/ebayAverage"
 @RestController
 public class EbaySneakerController {
 
     @RequestMapping(value = "/ebayAverage", method = RequestMethod.GET)
-    public EbayResponse handleEbayAverage(@RequestBody EbaySneaker ebaySneaker){
+    public EbayResponse handleEbayAverage(@RequestBody EbaySneaker ebaySneaker, HttpServletRequest request, HttpServletResponse response) throws IOException {
         boolean isValid = EbaySneaker.validateEbaySneaker(ebaySneaker);
 
-        if (isValid){
-            EbayThread thread = new EbayThread(ebaySneaker);
-            thread.run();
-            return new EbayResponse(thread.getPrice());
+        boolean isRedirectedSession = HelloController.validate_or_redirect_session(request, response);
+
+        if (isRedirectedSession) {
+            if (isValid) {
+                EbayThread thread = new EbayThread(ebaySneaker);
+                thread.run();
+                return new EbayResponse(thread.getPrice());
+            }
+            return new EbayResponse("NaN bad request");
         }
-        return new EbayResponse("NaN bad request");
+        return new EbayResponse("newSession");
     }
 }
