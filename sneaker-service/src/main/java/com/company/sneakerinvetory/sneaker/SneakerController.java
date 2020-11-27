95e472b25197cdc0551f44c6ca666a61c5bc4267
Terence21 <terencepeterson21@gmail.com>
2020-11-26 16:40:35 -0800

    added cross origin header

--- sneaker-service/src/main/java/com/company/sneakerinvetory/sneaker/SneakerController.java
+++ sneaker-service/src/main/java/com/company/sneakerinvetory/sneaker/SneakerController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 //curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{ "index": "1", "id" : "tester", "SneakerName" : "yeezy 350 v2", "Sku" : "fu9006", "size" : "11", "price" : "$350" }' "http://localhost:8080/editSneaker"
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class SneakerController {
 
