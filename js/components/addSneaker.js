792dd68059254c250df17a4193b750de7ad371fb
David Lee <tuc56947@temple.edu>
2020-12-06 16:35:40 -0500

    fixed success alert and redirect for add request

--- js/components/addSneaker.js
+++ js/components/addSneaker.js
@@ -28,7 +28,7 @@ function addSneaker () {
             if (this.readyState == 4 && this.status == 200){
                 console.log(this.responseText);
                 var result = JSON.parse(xmlhttp.response);
-                if (result.sessionID == 'sessionID'){
+                if (result.sessionID == 'SessionID'){
                     alert("Sneaker Added to Inventory");
                     window.location.hash = "#/sneakerInventory";
                 }else{
